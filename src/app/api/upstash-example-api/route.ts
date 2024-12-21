7a59b7d1ac7d630b59167e20803b0abccfff8baa
farooq-azam-khan <khanazamfarooq99@gmail.com>
2024-12-21 18:21:23 -0500

    rate limiting example

---
+++ src/app/api/upstash-example-api/route.ts
@@ -0,0 +1,37 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import "dotenv/config";
+import { NextResponse } from "next/server";
+
+const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL!,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+});
+
+// Create a new ratelimiter, that allows 5 requests per 5 seconds
+const ratelimit = new Ratelimit({
+    redis: redis,
+    limiter: Ratelimit.fixedWindow(5, "5 s"),
+});
+
+export async function GET() {
+    // Use a constant string to limit all requests with a single ratelimit
+    // Or use a userID, apiKey or ip address for individual limits.
+    const limit = await ratelimit.limit("anonymous");
+    if (!limit.success) {
+        return NextResponse.json(
+            {
+                status: "fail",
+                error: "rate limit reached",
+            },
+            { status: 429 },
+        );
+    }
+    return NextResponse.json(
+        {
+            status: "success",
+            message: "action happened successfully",
+        },
+        { status: 200 },
+    );
+}
