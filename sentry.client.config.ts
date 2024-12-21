a7dca66735d0b48e2c3f9cc99e371c55d1042bae
farooq-azam-khan <khanazamfarooq99@gmail.com>
2024-12-21 17:11:30 -0500

    user feedback integration

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,12 +3,16 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
-
 Sentry.init({
     dsn: "https://cba27723283f9bd8ce831a90b8062e9b@o4507636008091648.ingest.us.sentry.io/4508508278882304",
 
     // Add optional integrations for additional features
-    integrations: [Sentry.replayIntegration()],
+    integrations: [
+        Sentry.replayIntegration(),
+        Sentry.feedbackIntegration({
+            colorScheme: "system",
+        }),
+    ],
 
     // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
     tracesSampleRate: 1,
