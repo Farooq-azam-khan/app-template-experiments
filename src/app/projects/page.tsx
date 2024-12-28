16d6669abaf47d0caca6bb8ad6d10957d5696aea
farooq-azam-khan <khanazamfarooq99@gmail.com>
2024-12-28 11:43:58 -0500

    added sidebar

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,23 @@
 import { auth } from "@/auth";
 import { db } from "@/db";
+import { eq } from "drizzle-orm";
+import { projects } from "@/db/schema";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 async function get_user_projects(user_id: string) {
   // TODO: add a caching layer
   "use server";
   return await db.query.projects.findMany({
-    where: (projects, { eq }) => eq(user_id, projects.creator),
+    where: eq(projects.creator, user_id),
   });
 }
 
@@ -16,13 +28,31 @@ export default async function Projects() {
   }
   const user_projects = await get_user_projects(session.user.id);
   return (
-    <div>
+    <div className="mx-5 mt-10">
       <div>Projects for user {session.user.name}</div>
-      <div>
-        {user_projects.map((p) => (
-          <div key={p.id}>{p.project_name}</div>
-        ))}
-      </div>
+      <Table>
+        <TableCaption>A list of your recent projects.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">id</TableHead>
+            <TableHead>Name</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {user_projects.map((project) => (
+            <TableRow key={project.id}>
+              <TableCell className="font-medium">{project.id}</TableCell>
+              <TableCell>{project.project_name}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total Projects</TableCell>
+            <TableCell className="text-right">{user_projects.length}</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
     </div>
   );
 }
