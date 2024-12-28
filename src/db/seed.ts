fc9a17d695ef2d53e6245e19d40b8347796d5e15
farooq-azam-khan <khanazamfarooq99@gmail.com>
2024-12-28 12:16:19 -0500

    seeding

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,19 +1,31 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-import {
-  products,
-  users,
-  projects,
-  orders,
-  orderItems,
-  productCategories,
-  productReviews,
-} from "./schema";
+import { projects, productReviews, productCategories } from "./schema";
 import { seed } from "drizzle-seed";
 
 import "dotenv/config";
 async function main() {
-  const db = drizzle(process.env.DATABASE_URL!);
-  await seed(db, { productCategories });
+  console.log("Seeding started!");
+  const db_env = process.env.DATABASE_URL;
+  if (!db_env) {
+    console.error("no db env given");
+    return;
+  }
+  const db = drizzle(db_env);
+  await seed(db, { projects, productReviews, productCategories }).refine(
+    (f) => {
+      return {
+        projects: {
+          count: 100,
+          creator: f.valuesFromArray({ values: [1] }),
+        },
+      };
+    },
+  );
 }
 
-main();
+main()
+  .then()
+  .catch((err) => {
+    console.error(err);
+    process.exit(0);
+  });
